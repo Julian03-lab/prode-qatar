a7ebab4f8aa82701b45e538e20ccade97398a511
Julian <julian03yt@gmail.com>
2022-11-17 20:17:00 -0300

    Selection of first and secons of each group

---
+++ src/main.jsx
@@ -0,0 +1,10 @@
+import React from 'react'
+import ReactDOM from 'react-dom/client'
+import Fetching from './components/fetching'
+import "../node_modules/bootstrap/dist/css/bootstrap.min.css"
+
+ReactDOM.createRoot(document.getElementById('root')).render(
+  <>
+    <Fetching/>
+  </>
+)
